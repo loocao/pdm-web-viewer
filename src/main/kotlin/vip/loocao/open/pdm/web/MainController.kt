bbf6e3614120c1107bcec9e89d4d0dc42a19d344
loocao <dev@loocao.vip>
2017-10-13 11:27:26 +0800

    测试web

--- src/main/kotlin/vip/loocao/open/pdm/web/MainController.kt
+++ src/main/kotlin/vip/loocao/open/pdm/web/MainController.kt
@@ -1,8 +1,11 @@
 package vip.loocao.open.pdm.web
 
+import com.smshen.utils.Parser
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import vip.loocao.open.pdm.Application
 
 /**
  * @author loocao
@@ -13,7 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 class MainController {
 
     @GetMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
+        val pdmFileInputStream = Application::class.java.getResourceAsStream("/会员卡托管系统.pdm")
+        val parser = Parser()
+        val pdm = parser.pdmParser(pdmFileInputStream)
+
+        model.addAttribute("pdm", pdm)
+
         return "index"
     }
 }
